11571ccf2cd88dc8f1ac5ee5dfb5c6d55dd3f785
balqishafiraini <balqisshafiraaini@gmail.com>
2022-03-31 08:33:22 +0700

    Initial Commit

---
+++ mc1project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mc1project
+//
+//  Created by Balqis on 31/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
